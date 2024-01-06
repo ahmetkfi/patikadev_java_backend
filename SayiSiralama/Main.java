73eb90484b8d75f4cd89e4de5330ea59a8cb2275
ahmetkfi <ahmetkcygt58@gmail.com>
2024-01-07 01:29:04 +0300

    sayi siralama

---
+++ SayiSiralama/Main.java
@@ -0,0 +1,35 @@
+package SayiSiralama;
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("1. Sayiyi giriniz : ");
+        a=input.nextInt();
+        System.out.print("2. Sayiyi giriniz : ");
+        b=input.nextInt();
+        System.out.print("3. Sayiyi giriniz : ");
+        c=input.nextInt();
+
+        if ((a<b) && (a<c)){
+            if (b<c){
+                System.out.print(a+" < "+b+" < "+c);
+            } else{
+                System.out.print(a+" < "+c+" < "+b);
+            }
+        }else if ((b<a) && (b<c)){
+            if (a<c){
+                System.out.print(b+" < "+a+" < "+c);
+            }else {
+                System.out.print(b+" < "+c+" < "+a);
+            }
+        }else if ((c<a) && (c<b)){
+            if (b<a){
+                System.out.print(c+" < "+b+" < "+a);
+            }else {
+                System.out.print(c+" < "+a+" < "+b);
+            }
+        }
+    }
+}
